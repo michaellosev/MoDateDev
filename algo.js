1d4ae60c6c2ee24cdd517b3a93c935ed8eb72d9a
michael losev <ml5391@nyu.edu>
2021-09-30 14:33:34 -0400

    made the required changes

--- algo.js
+++ algo.js
@@ -307,26 +307,6 @@ async function generateMatches(prevMatches) {
   return girlMatches;
 }
 
-async function filterPrevMatches(prevDocs, document) {
-  const result = {}
-  for (let i = 0; i < prevDocs.length; i++) {
-    const sheet = documet.sheetsByTitle[prevDocs[i]]
-    const rows = await sheet.getRows();
-    rows.forEach(row => {
-      const arr = row['_rawData'];
-      const girlAlias = arr[4];
-      const guyAlias = arr[5];
-      if (result.hasOwnProperty(girlAlias)) {
-        result[girlAlias].push(guyAlias);
-      }
-      else {
-        result[girlAlias] = [guyAlias];
-      }
-    })
-  }
-  return result;
-}
-
 const getConnectors = async (title, document) => {
   const sheet = document.sheetsByTitle[title]
   const rows = await sheet.getRows();
@@ -418,40 +398,6 @@ const addMatches = async (title, prevMatches, fileName, document) => {
   })
 }
 
-// const allMatches = async (document) => {
-//   let re = /Matches/;
-//   const spreadSheetTitles = Object.keys(document.sheetsByTitle);
-//   let arrMatches = [];
-//   spreadSheetTitles.forEach(title => {
-//     if (re.test(title)) {
-//       arrMatches.push(title);
-//     }
-//   })
-//   const listOfMatches = {};
-//   for (let i = 0; i < arrMatches.length; i++) {
-//     const curSheet = doc.sheetsByTitle[arrMatches[i]];
-//     const rows = await curSheet.getRows();
-//     rows.forEach(row => {
-//       const girlAlias = row['_rawData'][4];
-//       const guyAlias = row['_rawData'][5];
-//       if (!listOfMatches.hasOwnProperty(girlAlias)) {
-//         listOfMatches[girlAlias] = {[guyAlias]: 1};
-//       }
-//       else {
-//         listOfMatches[girlAlias][guyAlias] = 1;
-//       }
-//       if (!listOfMatches.hasOwnProperty(guyAlias)) {
-//         listOfMatches[guyAlias] = {[girlAlias]: 1};
-//       }
-//       else {
-//         listOfMatches[guyAlias][girlAlias] = 1;
-//       }
-//     })
-//   }
-//   fs.writeFile('MatchesTest.json', JSON.stringify(listOfMatches, null, 2), (err) => {
-//     if (err) throw err;
-//   })
-// }
 
 // send mail to connectors
 const sendMail = process.argv[3];
